e16e83111342a7146760d986bb99cae02ec21d49
Zaid <zaidalqadiri@gmail.com>
2024-11-17 23:14:45 -0700

    updated handleAddTask function

--- src/tasks.jsx
+++ src/tasks.jsx
@@ -5,7 +5,10 @@ function Tasks() {
     const [tasks, setTasks] = useState(["Walk dog", "Grocery Shopping"]);
 
     function handleAddTask() {
+        const newTask = document.getElementById("textInput").value;
+        document.getElementById("textInput").value = "";
 
+        setTasks(t => [...t, newTask]);
     }
 
     function handleRemoveTask() {
@@ -21,8 +24,11 @@ function Tasks() {
     }
 
     return(<div>
-                <input type="text" placeholder="Enter a task" />
-                <button >Add</button>
+                <input type="text" placeholder="Enter a task" id="textInput"/>
+                <button onClick={handleAddTask}>Add</button>
+                <ul>
+                    {tasks.map((task, index) => <li key={index}>{task}</li>)}
+                </ul>
     
             </div>)
 
